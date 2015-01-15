4811e1cbaef23e4862154cfce648a2de3d6860b5
tiusty <awagud@hotmail.com>
2015-01-15 17:07:59 -0500

    Adding the inital main file to the master branch

---
+++ mastermind.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : masterMindMine.cpp
+// Author      : Alex Agudelo, Thurston Brevette
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+#include "Response.h"
+#include <vector>
+
+int main()
+{
+	Response r1;
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
