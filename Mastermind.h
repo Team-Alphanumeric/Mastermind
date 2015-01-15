d7fbb49a4ee8d5a3893468a9f060c82df2cfee47
tiusty <awagud@hotmail.com>
2015-01-15 17:43:51 -0500

    Added file Mastermind.h

---
+++ Mastermind.h
@@ -0,0 +1,26 @@
+/*
+ * Mastermind.h
+ *
+ *  Created on: Jan 15, 2015
+ *      Author: alex
+ */
+
+#ifndef MASTERMIND_H_
+#define MASTERMIND_H_
+#include "Code.h"
+#include "Response.h"
+
+class Mastermind {
+public:
+	Mastermind();
+	void printSecret();
+	void humanGuess();
+	virtual ~Mastermind();
+protected:
+	Code secret;
+	Code guess;
+	Response r1;
+
+};
+
+#endif /* MASTERMIND_H_ */
