1c0453618ee68ab69ec5f34dafff154e1a2d3bf1
tiusty <awagud@hotmail.com>
2015-01-16 18:16:50 -0500

    Adding main file which runs the program

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : masterMindMine.cpp
+// Author      : Alex Agudelo, Thurston Brevette
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+#include "Mastermind.h"
+
+int main()
+{
+	Mastermind game1;
+	game1.playGame();
+	return 0;
+}
