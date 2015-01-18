1784c6674888188e05aa4f7d33196708ca06124d
thorstone25 <thorstone25@users.noreply.github.com>
2015-01-17 19:08:39 -0500

    Copied partition of working version 2

--- Code.cpp
+++ Code.cpp
@@ -2,35 +2,31 @@
  * Code.cpp
  *
  *  Created on: Jan 15, 2015
- *      Author: alex
+ *      Author: alex, Thurston
  */
 
 #include "Code.h"
 #include <vector>
-using namespace std;
 #include <iostream>
 #include <cstdlib>
 #include <stdio.h>
+using namespace std;
 
-
+// create a code with random values
 Code::Code()
 {
-	cd.resize(5);
-	int test;
-	// TODO Auto-generated constructor stub
-	 for(int ii=0;ii<4;ii++)
-	 {
-		 cd[ii] = (int) rand() % 6;
-	 }
+	setRandomCode();
 }
-Code::Code(int arr[])
+
+// create a code with passed values
+Code::Code(const int arr[])
 {
 	 setCode(arr);
 }
-Code::~Code() {
-	// TODO Auto-generated destructor stub
-}
-void Code::setCode(int arr[])
+
+
+// set the code values to the array values
+void Code::setCode(const int arr[])
 {
 	cd.resize(4);
 	for(int ii=0;ii<4;ii++)
@@ -38,13 +34,38 @@ void Code::setCode(int arr[])
 		cd[ii] = arr[ii];
 	}
 }
+
+// set random values for the code
+void Code::setRandomCode()
+{
+	cout << "Setting Random Code for Secret Code.\n";
+	cd.resize(4);
+	for(int ii=0;ii<4;ii++)
+	{
+		 cd[ii] = (int) rand() % 6;
+	}
+}
+
+// get the indexed values in a code
 const int Code::getVal(const int index)
 {
 	return cd[index];
 }
+
+
+// print the digits in the code
+const void Code::printCode()
+{
+	for(int ii=0;ii<4;ii++)
+	{
+		cout << cd[ii];
+	}
+	cout << "\n";
+}
+
 // Checks the number of correct guesses which involve the guess having a correct number
 // in the correct position. Assumes that the parameter passed to the function is the guess.
-int Code::checkCorrect(Code gs)
+const int Code::checkCorrect(Code gs)
 {
 	// count the number of equal values in corresponding position
 	int temp=0;
@@ -61,16 +82,16 @@ int Code::checkCorrect(Code gs)
 // Checks the number of incorrect guesses which involve the guess having a correct number
 // in the wrong position. Only reports a maximum of one correct value for each position in
 // the code. Assumes that the parameter passed to the function is the guess.
-
-int Code::checkIncorrect(Code gs)
+const int Code::checkIncorrect(Code gs)
 {
 	// create image of guess corresopnding to code vectors
-	bool image[4] = {false,false,false,false};
+	 int image[4] = {0,0,0,0};
+
 	// iteratively check values from the secret
 	for(int ii=0;ii<4;ii++) // ii is the secret index
 	{
 		// check only if secret index isn't a correct match
-		if(cd[ii]!=gs.getVal(ii))
+		if(cd[ii] != gs.getVal(ii))
 		{
 			// iteratively compare to values from the guess
 			for(int jj=0;jj<4;jj++) // jj is the guess index
@@ -81,30 +102,24 @@ int Code::checkIncorrect(Code gs)
 				{
 					if(cd[ii] == gs.getVal(jj))
 					{
-						image[jj] = true; break;
+						image[jj] = 1; break;
 					}
 				}
 			}
 		}
+		else 
+		{ image[ii]=-1; } // indicate matches by setting image to -1
 	}
-	// count the number of denoted matches in the image
-	int temp=0; for(int kk=0;kk<4;kk++) { if(image[kk]) {temp++;} }
+
+	
+	// count the number of denoted incorrects in the image
+	int temp=0; for(int kk=0;kk<4;kk++) { if(image[kk]>0) {temp++;} }
+	
 	// return number of matches
 	return temp;
 }
-void Code::setRandomCode()
-{
-	cout << "Setting Random Code for Secret Code.\n";
-	for(int ii=0;ii<4;ii++)
-	 {
-		 cd[ii] = (int) rand() % 6;
-	 }
-}
-void Code::printCode()
-{
-	for(int ii=0;ii<4;ii++)
-	{
-		cout << cd[ii];
-	}
-	cout << "\n";
-}
+
+// deconstrucor stub
+Code::~Code() {}	// TODO Auto-generated destructor stu
+
+                                       
