f357333095429177938e0b8ed31b7702207c33d7
thorstone25 <thorstone25@users.noreply.github.com>
2015-01-17 19:10:48 -0500

    Copied partition of working version 2

--- Response.cpp
+++ Response.cpp
@@ -2,14 +2,15 @@
  * Response.cpp
  *
  *  Created on: Jan 15, 2015
- *      Author: alex
+ *      Author: alex, Thurston
  */
 
 #include "Response.h"
-using namespace std;
 #include <iostream>
+using namespace std;
 
-//constructor method for the Response class
+
+// creates a default response
 Response::Response()
 {
 	//sets the numCorrect and numIncorrect to -1 for debugging purposes
@@ -17,66 +18,66 @@ Response::Response()
 	numCorrect = -1;
 	numIncorrect = -1;
 }
-//This function checks to see if the two codes are the same and outputs true if they are the same
-//and outputs false if they are differnt
-//this is the same as the numCorrect method and if the output is not 4 then they are different
 
-bool Response::checkSame()
-{
-	//if the numCorrect=4 then the two responses are the same
-	if(numCorrect ==4)
-	{
-		//returns true if the numCorrect =4 because they are equal
-		return true;
-	}
-	else
-	{
-		//prints out the number of correct and incorrect digits
-		cout << "The number of digits in the correct position is " << numCorrect << ".\n";
-		cout << "The number of digits in the incorrect position " << numIncorrect << ".\n";
-	}
-	//otherwise return false if the numCorrect is not equal to 4
-	return false;
-}
-void Response::printNumCorrect()
-{
-	cout << "The number of digits in the correct position is " << numCorrect << ".\n";
-}
-bool Response::checkWin()
-{
-	//to check if the user won, it checks to see if the numCorrect is 4, if it is then the
-	//reponses are the same so the guesser won
-	if(numCorrect == 4)
-	{
-		//returns true which indicates the human guessed the code
-		cout << "You have guessed the right code!!!! You won!! now play again...." << ".\n";
-		return true;
-	}
-	cout << "Well you guessed it wrong...try again...\n";
-	//returns false which indicates that the human as not guessed the secret code yet
-	return false;
-}
 //get function that returns the number of Correct positions
-int Response::getCorrect()
+const int Response::getCorrect()
 {
 	return numCorrect;
 }
+
 //get function that resturns the number of Incorrect positions
-int Response::getIncorrect()
+const int Response::getIncorrect()
 {
 	return numIncorrect;
 }
+
 //set function to manually set the value of numCorrect
 void Response::setNumCorrect(int const newNumCorrect)
 {
-	numCorrect = newNumCorrect;
+	numCorrect = newNumCorrect; return;
 }
+
 //set function to manually set the value of incorrect
 void Response::setNumIncorrect(int const newNumIncorrect)
 {
-	numIncorrect = newNumIncorrect;
+	numIncorrect = newNumIncorrect; return;
 }
-Response::~Response() {
-	// TODO Auto-generated destructor stub
+
+// print the response
+const void Response::printResponse()
+{
+	cout << "There are "<<numCorrect<<" digits in the correct position and "<<numIncorrect<<" in the wrong position.\n";
 }
+
+//This function checks to see if the two codes are the same and outputs true if they are the same
+//and outputs false if they are different
+const bool Response::checkSame(Response r)
+{
+	return (numCorrect == r.getCorrect()) && (numIncorrect == r.getIncorrect());
+}
+
+const bool Response::checkWin(const bool announce)
+{
+	//to check if the user won, it checks to see if the numCorrect is 4
+	if(!announce) { return numCorrect==4;	}
+	else
+	{
+	if(numCorrect == 4)
+		{
+			//returns true which indicates the human guessed the code
+			cout << "You have guessed the right code!!!! You won!! now play again...." << ".\n";
+			return true;
+		}
+		else 
+		{
+			//returns false which indicates that the human as not guessed the secret code yet
+			cout << "Well you guessed it wrong...try again...\n";
+			return false;
+		}
+	}
+}
+
+// deconstructor stub
+Response::~Response()
+{	/* TODO Auto-generated destructor stub */ }
 
